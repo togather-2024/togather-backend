cb4f7651450c7b248115c3e6f5b2fd09405f9ead
sejyom <akxxkd01@gmail.com>
2024-02-17 20:47:25 +0900

    Feat: Added member-related table

---
+++ sql/member_related_table.sql
@@ -0,0 +1,20 @@
+-- 회원
+CREATE TABLE `member` (
+                          `member_srl` bigint NOT NULL,
+                          `member_name` varchar(50) NOT NULL,
+                          `password` varchar(50) NOT NULL,
+                          `role` varchar(50) NOT NULL,
+                          `email` varchar(50) NOT NULL UNIQUE,
+                          `profile_pic_file` varchar(50),
+                          `email_verified` boolean,
+                          PRIMARY KEY (`member_srl`)
+);
+
+-- 이메일 인증
+CREATE TABLE `email_verification` (
+                                      `email_verification_id` bigint NOT NULL,
+                                      `email` varchar(50) NOT NULL,
+                                      `verification_code` varchar(50) NOT NULL,
+                                      `verification_expiration_time` datetime NOT NULL,
+                                      PRIMARY KEY (`email_verification_id`)
+);
