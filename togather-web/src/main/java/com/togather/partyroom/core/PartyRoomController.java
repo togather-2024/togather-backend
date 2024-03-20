7d0135df04669d41b9e0dfdbbdbfeefe2f7acd04
최대훈 <37106166+david1403@users.noreply.github.com>
2024-03-20 18:58:39 +0900

    Feature: modify param for party room CRUD API (#30)

--- togather-web/src/main/java/com/togather/partyroom/core/PartyRoomController.java
+++ togather-web/src/main/java/com/togather/partyroom/core/PartyRoomController.java
@@ -1,5 +1,6 @@
 package com.togather.partyroom.core;
 
+import com.togather.common.AddJsonFilters;
 import com.togather.member.model.MemberDto;
 import com.togather.member.service.MemberService;
 import com.togather.partyroom.core.model.PartyRoomDetailDto;
@@ -27,6 +28,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.togather.common.ResponseFilter.MEMBER_DTO_EXCLUDE_PII;
+
 @RequiredArgsConstructor
 @Controller
 @Tag(name = "Party Room Core CRUD")
@@ -40,34 +43,36 @@ public class PartyRoomController {
     @Operation(summary = "partyRoom creation(Registration) API", description = "파티룸 등록 API")
     @ApiResponse(responseCode = "200", description = "success", content = @Content(schema = @Schema(implementation = PartyRoomDto.class)))
     @ApiResponse(responseCode = "401", description = "user not logged in (No JWT token)", content = @Content)
-    @ApiResponse(responseCode = "403", description = "has no role or is not owner of party room", content = @Content)
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(
-            description = "required in partyRoomDto: partyRoomName, desc, opening/closing hour, price, guestCapacity.<br>" +
-                    "required in customTags: tagContents"
-    )
-    public ResponseEntity<PartyRoomDto> register(@Valid @RequestBody PartyRoomRequestDto partyRoomRequestDto) {
+    @ApiResponse(responseCode = "403", description = "user is logged in but has no HOST role", content = @Content)
+    @AddJsonFilters(filters = MEMBER_DTO_EXCLUDE_PII)
+    public MappingJacksonValue register(@Valid @RequestBody PartyRoomRequestDto partyRoomRequestDto) {
 
-        PartyRoomDto partyRoomDto = partyRoomRequestDto.getPartyRoomDto();
+        PartyRoomDto partyRoomDto = partyRoomRequestDto.extractPartyRoomDto();
         // DONE: extract from JWT token
         MemberDto partyRoomHost = memberService.findByAuthentication(SecurityContextHolder.getContext().getAuthentication());
 
-        PartyRoomLocationDto partyRoomLocationDto = partyRoomRequestDto.getPartyRoomLocationDto();
-        List<PartyRoomCustomTagDto> customTags = partyRoomRequestDto.getCustomTags();
-        List<PartyRoomOperationDayDto> operationDayDtos = partyRoomRequestDto.getOperationDays();
-        PartyRoomImageDto partyRoomMainImageDto = partyRoomRequestDto.getPartyRoomImageDto();
+        PartyRoomLocationDto partyRoomLocationDto = partyRoomRequestDto.extractPartyRoomLocationDto();
+        List<PartyRoomCustomTagDto> customTags = partyRoomRequestDto.extractCustomTags();
+        List<PartyRoomOperationDayDto> operationDays = partyRoomRequestDto.extractOperationDays();
+        PartyRoomImageDto partyRoomMainImageDto = partyRoomRequestDto.extractPartyRoomImageDto();
 
         partyRoomDto.setPartyRoomHost(partyRoomHost);
 
-        PartyRoomDto registeredPartyRoom = partyRoomService.register(partyRoomDto, customTags, partyRoomLocationDto, partyRoomMainImageDto, operationDayDtos);
+        PartyRoomDto registeredPartyRoom = partyRoomService.register(partyRoomDto, customTags, partyRoomLocationDto, partyRoomMainImageDto, operationDays);
 
-        return ResponseEntity.ok(registeredPartyRoom);
+        return new MappingJacksonValue(registeredPartyRoom);
     }
 
     @PostMapping("/modify")
     @PreAuthorize("hasRole('ROLE_HOST')")
     @ResponseBody
-    public ResponseEntity<PartyRoomDto> modify(@Valid @RequestBody PartyRoomRequestDto partyRoomRequestDto) {
-        PartyRoomDto partyRoomDto = partyRoomService.findPartyRoomDtoById(partyRoomRequestDto.getPartyRoomDto().getPartyRoomId());
+    @Operation(summary = "partyRoom modification API", description = "파티룸 정보 수정 API")
+    @ApiResponse(responseCode = "200", description = "success", content = @Content(schema = @Schema(implementation = PartyRoomDto.class)))
+    @ApiResponse(responseCode = "401", description = "user not logged in (No JWT token)", content = @Content)
+    @ApiResponse(responseCode = "403", description = "has no role or is not owner of party room", content = @Content)
+    @AddJsonFilters(filters = MEMBER_DTO_EXCLUDE_PII)
+    public MappingJacksonValue modify(@Valid @RequestBody PartyRoomRequestDto partyRoomRequestDto) {
+        PartyRoomDto partyRoomDto = partyRoomService.findPartyRoomDtoById(partyRoomRequestDto.extractPartyRoomDto().getPartyRoomId());
 
         MemberDto loginUser = memberService.findByAuthentication(SecurityContextHolder.getContext().getAuthentication());
         // Compare user from token & user stored in DB. If different, the user is trying to modify a different user's party room
@@ -75,18 +80,22 @@ public class PartyRoomController {
             throw new AccessDeniedException("cannot modify different user's party room");
         }
 
-        PartyRoomLocationDto partyRoomLocationDto = partyRoomRequestDto.getPartyRoomLocationDto();
-        List<PartyRoomCustomTagDto> customTags = partyRoomRequestDto.getCustomTags();
-        List<PartyRoomOperationDayDto> operationDayDtos = partyRoomRequestDto.getOperationDays();
-        PartyRoomImageDto partyRoomMainImageDto = partyRoomRequestDto.getPartyRoomImageDto();
+        PartyRoomLocationDto partyRoomLocationDto = partyRoomRequestDto.extractPartyRoomLocationDto();
+        List<PartyRoomCustomTagDto> customTags = partyRoomRequestDto.extractCustomTags();
+        List<PartyRoomOperationDayDto> operationDays = partyRoomRequestDto.extractOperationDays();
+        PartyRoomImageDto partyRoomMainImageDto = partyRoomRequestDto.extractPartyRoomImageDto();
 
-        PartyRoomDto registeredPartyRoom = partyRoomService.modifyPartyRoom(partyRoomDto, customTags, partyRoomLocationDto, partyRoomMainImageDto, operationDayDtos);
-        return ResponseEntity.ok(registeredPartyRoom);
+        PartyRoomDto registeredPartyRoom = partyRoomService.modifyPartyRoom(partyRoomDto, customTags, partyRoomLocationDto, partyRoomMainImageDto, operationDays);
+        return new MappingJacksonValue(registeredPartyRoom);
     }
 
     @PostMapping("/delete/{id}")
     @PreAuthorize("hasRole('ROLE_HOST')")
     @ResponseBody
+    @Operation(summary = "partyRoom deletion API", description = "파티룸 삭제 API")
+    @ApiResponse(responseCode = "200", description = "returns response with string 'ok' when deleted successfully")
+    @ApiResponse(responseCode = "401", description = "user not logged in (No JWT token)", content = @Content)
+    @ApiResponse(responseCode = "403", description = "has no role or is not owner of party room", content = @Content)
     public ResponseEntity<String> delete(@PathVariable("id") long partyRoomId) {
         PartyRoomDto partyRoomDto = partyRoomService.findPartyRoomDtoById(partyRoomId);
         MemberDto loginUser = memberService.findByAuthentication(SecurityContextHolder.getContext().getAuthentication());
@@ -101,8 +110,13 @@ public class PartyRoomController {
     @GetMapping("/detail/{id}")
     @PreAuthorize("hasRole('ROLE_HOST')")
     @ResponseBody
-    public ResponseEntity<PartyRoomDetailDto> getPartyRoomDetail(@PathVariable("id") long partyRoomId) {
-        return ResponseEntity.ok(partyRoomService.findDetailDtoById(partyRoomId));
+    @Operation(summary = "partyRoom detail info API", description = "파티룸 조회 API")
+    @ApiResponse(responseCode = "200", description = "returns response with string 'ok' when deleted successfully")
+    @ApiResponse(responseCode = "401", description = "user not logged in (No JWT token)", content = @Content)
+    @ApiResponse(responseCode = "403", description = "has no role or is not owner of party room", content = @Content)
+    @AddJsonFilters(filters = MEMBER_DTO_EXCLUDE_PII)
+    public MappingJacksonValue getPartyRoomDetail(@PathVariable("id") long partyRoomId) {
+        return new MappingJacksonValue(partyRoomService.findDetailDtoById(partyRoomId));
     }
 
 }
