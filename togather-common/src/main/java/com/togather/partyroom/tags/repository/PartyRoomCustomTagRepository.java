e79c2a0566a657fd7951dcdc25f70ef2b5797f33
david1403 <david1403@hanmail.net>
2024-03-30 16:04:19 +0900

    Feature: add minor changes/APIs for requirements

--- togather-common/src/main/java/com/togather/partyroom/tags/repository/PartyRoomCustomTagRepository.java
+++ togather-common/src/main/java/com/togather/partyroom/tags/repository/PartyRoomCustomTagRepository.java
@@ -1,13 +1,14 @@
 package com.togather.partyroom.tags.repository;
 
-import com.togather.partyroom.core.model.PartyRoom;
 import com.togather.partyroom.tags.model.PartyRoomCustomTag;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
+import java.awt.print.Pageable;
 import java.util.List;
-import java.util.Optional;
 
 public interface PartyRoomCustomTagRepository extends JpaRepository<PartyRoomCustomTag, Long> {
     PartyRoomCustomTag findByTagContent(String tagContent);
+
+    List<PartyRoomCustomTag> findAllByOrderByTagCountDesc(Limit limit);
 }
