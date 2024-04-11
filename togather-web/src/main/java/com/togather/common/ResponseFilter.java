9d8657ad68900756b9bf53ce5a4a05855098995b
sejyom <akxxkd01@gmail.com>
2024-04-11 14:54:43 +0900

    fix: modify to return Member PK when querying a single party room reservation

--- togather-web/src/main/java/com/togather/common/ResponseFilter.java
+++ togather-web/src/main/java/com/togather/common/ResponseFilter.java
@@ -17,7 +17,7 @@ public enum ResponseFilter {
 //    PARTY_ROOM_RESERVATION_DTO(PartyRoomReservationDto.class, new String[]{}),
     PARTY_ROOM_RESERVATION_RESPONSE_DTO_SIMPLE(PartyRoomReservationResponseDto.class, new String[]{"reservationGuestDto", "guestCount"}),
     PARTY_ROOM_IMAGE_DTO_SIMPLE(PartyRoomImageDto.class, new String[]{"partyRoomImageId", "partyRoomImageType"}),
-    MEMBER_DTO_FOR_RESERVATION(MemberDto.class, new String[]{"memberSrl", "password", "role", "email"}),
+    MEMBER_DTO_FOR_RESERVATION(MemberDto.class, new String[]{"password", "role", "email"}),
     PAYMENT_INFO(PaymentDto.class, new String[]{"paymentId", "createdAt", "paymentKey"}),
     ;
 
